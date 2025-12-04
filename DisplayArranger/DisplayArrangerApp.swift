6289517899ceb508acc2422bf72db3f4ca2e1753
Ole Christian <olechr.sollid@icloud.com>
2025-12-04 13:55:11 +0100

    Initial Commit

---
+++ DisplayArranger/DisplayArrangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayArrangerApp.swift
+//  DisplayArranger
+//
+//  Created by Ole Christian Sollid on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DisplayArrangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisplayArrangerTests/DisplayArrangerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayArrangerTests.swift
+//  DisplayArrangerTests
+//
+//  Created by Ole Christian Sollid on 04/12/2025.
+//
+
+import Testing
+@testable import DisplayArranger
+
+struct DisplayArrangerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
